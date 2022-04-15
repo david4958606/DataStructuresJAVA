ec9c033414f98cf47c2e63b8e4ff7e89ead1940d
David Wang <david.wang@kirinou.top>
2022-04-15 15:21:43 +0800

    feat(List): ✨ Implement linkedlist in JAVA
    尝试实现链表

---
+++ README.md
@@ -0,0 +1,9 @@
+# Data Structures and Algorithm Analysis in Java
+
+Learning Data Structures and Algorithm Analysis in Java.
+
+## Progression
+
+- [ ] List ADT
+- [ ] Stack ADT
+- [ ] Queue ADT
--- src/top/kirinou/list/MylinkedList.java
+++ src/top/kirinou/list/MylinkedList.java
@@ -1,2 +1,143 @@
-package top.kirinou.list;public class MylinkedList {
+package top.kirinou.list;
+
+public class MylinkedList<AnyType> implements Iterable<AnyType> {
+    private static class Node<AnyType> {
+        public Node(AnyType data, Node<AnyType> prev, Node<AnyType> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+        public AnyType data;
+        public Node<AnyType> next;
+        public Node<AnyType> prev;
+    }
+
+    public MylinkedList() {doClear();}
+
+    public void clear() {doClear();}
+
+    private void doClear() {
+        beginMarker = new Node<AnyType>(null, null, null);
+        endMarker = new Node<AnyType>(null, beginMarker, null);
+        beginMarker.next = endMarker;
+
+        theSize = 0;
+        modCount++;
+    }
+
+    public int size() {return theSize;}
+
+    public boolean isEmpty() {return size() == 0;}
+
+    public boolean add(AnyType x) {
+        add(size(), x);
+        return true;
+    }
+
+    public void add(int idx, AnyType x) {
+        addBefore(getNode(idx, 0, size()), x);
+    }
+
+    public AnyType get(int idx) {
+        return getNode(idx).data;
+    }
+
+    public AnyType set(int idx, AnyType newVal) {
+        Node<AnyType> p = getNode(idx);
+        AnyType oldVal = p.data;
+        p.data = newVal;
+        return oldVal;
+    }
+
+    public AnyType remove(int idx) {
+        return remove(getNode(idx));
+    }
+
+    /**
+     * Add an item to the list, at specified position p.
+     * Items at or after that position are slid one position higher.
+     * @param p Node to add before.
+     * @param x any Object.
+     * @throws IndexOutOfBoundsException if p is not a valid position.
+     **/
+    private void addBefore(Node<AnyType> p, AnyType x) {
+        Node<AnyType> newNode = new Node<>(x, p.prev, p);
+        newNode.prev.next = newNode;
+        p.prev = newNode;
+        theSize++;
+        modCount++;
+    }
+
+    /**
+     * Removes the object contained in Node p
+     * @param p Node to remove.
+     * @return the object contained in Node p.
+     * **/
+    private AnyType remove(Node<AnyType> p) {
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    private Node<AnyType> getNode(int idx) {
+        return getNode(idx, 0, size() - 1);
+    }
+
+    private Node<AnyType> getNode(int idx, int lower, int upper) {
+        Node<AnyType> p;
+        if (idx < lower || idx > upper)
+            throw new IndexOutOfBoundsException();
+        if (idx < size() / 2) {
+            p = beginMarker.next;
+            for (int i = 0; i < idx; i++)
+                p = p.next;
+        } else {
+            p = endMarker;
+            for (int i = size(); i > idx; i--)
+                p = p.prev;
+        }
+        return p;
+    }
+
+    public java.util.Iterator<AnyType> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements java.util.Iterator<AnyType> {
+        private Node<AnyType> current = beginMarker.next;
+        private int expectedModCount = modCount;
+        private boolean okToRemove = false;
+
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        public AnyType next() {
+            if (!hasNext())
+                throw new java.util.NoSuchElementException();
+            if (modCount != expectedModCount)
+                throw new java.util.ConcurrentModificationException();
+            AnyType nextItem = current.data;
+            current = current.next;
+            okToRemove = true;
+            return nextItem;
+        }
+
+        public void remove() {
+            if (!okToRemove)
+                throw new IllegalStateException();
+            if (modCount != expectedModCount)
+                throw new java.util.ConcurrentModificationException();
+            MylinkedList.this.remove(current.prev);
+            expectedModCount++;
+            okToRemove = false;
+        }
+    }
+
+    private int theSize;
+    private int modCount = 0;
+    private Node<AnyType> beginMarker;
+    private Node<AnyType> endMarker;
 }
